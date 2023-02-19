3872762fb971a7a1cb1cca8aa72cd36b2c47a89d
André Luiz Perensin <andreluizperensin@gmail.com>
2023-02-19 15:42:47 -0300

    refactored user repository update method to use postgres database

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -66,7 +66,7 @@ class ContactController {
 
     const contactByEmail = await ContactsRepository.findByEmail(email);
 
-    if (contactByEmail && contactByEmail.id === id) {
+    if (contactByEmail && contactByEmail.id !== id) {
       return response.status(400).json({ error: 'This e-mail is already in use' });
     }
 

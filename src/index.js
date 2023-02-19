412496b6fe0927d92b2de4b60282d8441cccae3a
André Luiz Perensin <andreluizperensin@gmail.com>
2023-02-19 18:05:11 -0300

    added erro handler and express-async-errors package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+require('express-async-errors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use((error, _request, response, _next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));

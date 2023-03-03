e16a78d22ce94587c0cca7fe72fad5014d09bcac
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-02 21:38:47 -0300

    added header response into cors middleware: Access-Control-Allow-Methods / Access-Control-Allow-Headers

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -2,5 +2,7 @@ module.exports = (_request, response, next) => {
   // Wildcard -> Curinga -> *
   // response.setHeader('Access-Control-Allow-Origin', '*');
   response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
   next();
 };

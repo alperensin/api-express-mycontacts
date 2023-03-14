e2bea034c7dd14ea1d1523acc156ebe295591af0
André Luiz Perensin <andreluizperensin@gmail.com>
2023-03-13 21:58:32 -0300

    releasing multiple origins in cors

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,9 +1,23 @@
-module.exports = (_request, response, next) => {
+module.exports = (request, response, next) => {
+  const allowedOrigins = [
+    'http://192.168.50.102:3000',
+    'http://localhost:3000',
+  ];
+
+  const origin = request.header('origin');
+
+  const isAllowed = allowedOrigins.includes(origin);
+
   // Wildcard -> Curinga -> *
-  // response.setHeader('Access-Control-Allow-Origin', '*');
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '10');
+  if (isAllowed) {
+    // response.setHeader('Access-Control-Allow-Origin', '*');
+    // response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // response.setHeader('Access-Control-Allow-Origin', 'http://192.168.50.102:3000');
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '10');
+  }
+
   next();
 };
